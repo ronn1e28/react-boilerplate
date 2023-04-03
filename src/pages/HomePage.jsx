f27e3e64894edcc1d0fc101e8bc637ff552e5ee0
Rohan Deshpande <rohan_4000_deshpande@hotmail.com>
2023-04-03 22:14:46 +0530

    initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HomeComponent from "../Components/HomeComponent";
+
+const HomePage = () => {
+  return (
+    <div>
+      Home Page
+      <HomeComponent />
+    </div>
+  );
+};
+
+export default HomePage;
